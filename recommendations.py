8f8adbb8b261a49f8e3edbf4148c39ccda9e8f65
Pkafei <plburton@gmail.com>
2013-08-07 13:45:18 -0700

    Added perosnalized movie recommendations

--- recommendations.py
+++ recommendations.py
@@ -2,6 +2,7 @@
 from math import sqrt
 
 critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on the Plane': 3.5,
+
     'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You Me and Dupree': 2.5,
     'The Night Listener': 3.0},
     'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
@@ -73,3 +74,34 @@ def topMatches(prefs, person, n=5, similarity=sim_pearson):
     scores.sort()
     scores.reverse()
     return scores [0:n]
+
+# Gets recommendations for a person by using a weighted average
+# of every other user's rankings
+def getRecommendations(prefs, person, similarity=sim_pearson):
+    totals={}
+    simSums={}
+    for other in prefs:
+        # don't compare me to myself
+        if other == person: continue
+        sim = similarity(prefs, person, other)
+
+        # ignore scores of zero or lower
+        if sim <=0 : continue
+        for item in prefs[other]:
+
+            # only score movies I haven't seen yet
+            if item not in prefs[person] or prefs[person][item] == 0:
+                # Similarity * Score
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+                # Sum of similarities
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
+    
+    # Create Normalized List
+    rankings = [(total/simSums[item], item) for item, total in totals.items()]
+
+    # Return the sorted list
+    rankings.sort()
+    rankings.reverse()
+    return rankings
