e54f67aeb66f3bd3e269c3592e20955a0af44c02
Pkafei <plburton@gmail.com>
2013-08-07 14:12:33 -0700

    Transform similarities between users and movies

--- recommendations.py
+++ recommendations.py
@@ -105,3 +105,14 @@ def getRecommendations(prefs, person, similarity=sim_pearson):
     rankings.sort()
     rankings.reverse()
     return rankings
+
+# Transform similarities between users to between movies
+def transformPref(prefs):
+    result = {}
+    for person in prefs:
+        for item in prefs[person]:
+            result.setdefault(item,{})
+
+            # Flip item and person
+            result[item][person] = prefs[person][item]
+    return result
